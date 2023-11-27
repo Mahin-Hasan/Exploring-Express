30572f5ec33738a402604e267130b7eaca0d50bb
Mahin-Hasan <mahinmtrs@gmail.com>
2023-11-28 02:44:36 +0600

    implemented post methond

---
+++ __steps__.js
@@ -0,0 +1,9 @@
+/* 
+ 1. Create a post api on th eserver side
+ 2. Client side send data by a post  
+ 3. set fetch method inside the fetch options (2nd parameter)
+ 4. options will have three things: methond
+ 5. options will have headers: 'content-type': 'application/json'
+ 6. send data in JSON.stringify in the body
+ 7. don't forget to use express.json() middle ware int the server side
+ */
--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000; // for avoiding deployment related issues
+
+//middleware
+app.use(cors());
+app.use(express.json());
 
 
 const users = [
@@ -15,6 +20,15 @@ app.get('/', (req, res) => {
 
 app.get('/users', (req, res) => {
     res.send(users);
+})
+
+app.post('/users', (req, res) => {
+    console.log('post api hitted')
+    console.log(req.body);
+    const newUser = req.body;
+    newUser.id = users.length + 1;
+    users.push(newUser);
+    res.send(newUser);
 })
 
 app.listen(port, () => {
